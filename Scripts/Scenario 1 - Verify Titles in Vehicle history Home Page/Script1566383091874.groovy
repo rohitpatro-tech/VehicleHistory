9482dfdbf7b6926ba47034f8952837c8d016c70b
techolution <techolution@192.168.3.152>
2019-09-10 18:27:26 +0530

    change1-shabareshh

--- Scripts/Scenario 1 - Verify Titles in Vehicle history Home Page/Script1566383091874.groovy
+++ Scripts/Scenario 1 - Verify Titles in Vehicle history Home Page/Script1566383091874.groovy
@@ -26,7 +26,7 @@ WebUI.openBrowser('https://www.vehicleHistory.com')
 
 WebUI.maximizeWindow()
 
-WebUI.waitForPageLoad(50)
+WebUI.waitForPageLoad(40)
 
 WebDriver driver = DriverFactory.getWebDriver()
 
